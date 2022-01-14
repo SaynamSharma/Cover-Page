6c3b9bc2b20cfe704f95e7b20466f4d5350f4532
SaynamSharma <saynamsharma221b@gmail.com>
2022-01-14 22:46:39 +0530

    Update settings.py

--- webb/settings.py
+++ webb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k5@is&1pmj_-f8dwl+tb+imycztxa=f=dbrq_u5^lm$d3g=+r('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['saynam.herokuapp.com','127.0.0.1']
 
