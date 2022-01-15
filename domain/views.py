11c65cfdc0abdabc0ca119da2382c6b67a9bb425
SaynamSharma <saynamsharma221b@gmail.com>
2022-01-15 12:32:09 +0530

    h

--- domain/views.py
+++ domain/views.py
@@ -1,3 +1,4 @@
+from ast import Name
 from django.shortcuts import render , HttpResponse
 from datetime import datetime
 from domain.models import Contact
@@ -11,8 +12,8 @@ def contact(request):
     if request.method == "POST": 
         name = request.POST.get('name')
         email = request.POST.get('email')
-        sugs = request.POST.get('sugs')
+        desc = request.POST.get('desc')
         phone = request.POST.get('phone')
-        contact =  Contact(name=name, email=email, sugs=sugs, phone=phone, date = datetime.today() )
+        contact =  Contact(name=name, email=email, desc=desc, phone=phone, date=datetime.today())
         contact.save()
     return render (request, "contact.html")
